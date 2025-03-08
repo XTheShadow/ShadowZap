0e69a836f11c8e5b2e3e01a7df0e20573e1603c4
XTheShadow <theshadowsamer@gmail.com>
2025-03-08 23:39:05 +0300

    Added and defined  an Enum class

--- app/api/scan.py
+++ app/api/scan.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel # This library is used to validate the incoming data
+from enum import Enum          # A python special class that represents a fixed set of values or options
+
+# A class defining the fixed terms or options so no invalid terms are used
+class ScanType(Enum):
+    BASIC = "basic"
+    FULL = "full"
+    PORT_SCAN = "port_scan"
+    VULNERABILITY = "vulnerability"
+
 
 # Creating a class to validate the incoming data
 class ScanRequest(BaseModel):
