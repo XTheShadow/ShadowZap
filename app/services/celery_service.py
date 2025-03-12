34f516dd320f180e2048bc4a51e927c5cf738343
XTheShadow <theshadowsamer@gmail.com>
2025-03-12 17:36:19 +0300

    A services setting folder was created

---
+++ app/services/celery_service.py
@@ -0,0 +1,7 @@
+# This is for setting the Celery service.
+from celery import Celery
+
+celery = Celery(
+    'Automated_Pen_Testing',
+    broker= 'redis://localhost:6379' # The port "6379" is the standard port for Redis 
+)
