5f74f721237de981a71bc64277df1f7b991a1815
XTheShadow <theshadowsamer@gmail.com>
2025-03-15 18:56:50 +0300

    Added a url validation decorator

--- app/models/scan_model.py
+++ app/models/scan_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, AnyHttpUrl # This library is used to validate the incoming data
+from pydantic import BaseModel, AnyHttpUrl, validator # This library is used to validate the incoming data
 from enum import Enum          # A python special class that represents a fixed set of values or options
 
 # A class defining the fixed terms or options so no invalid terms are used
@@ -14,3 +14,7 @@ class ScanRequest(BaseModel):
     scan_type: ScanType = ScanType.BASIC  # Changed to use the "ScanType" class so there is no invalid entries
     ports: str = "1-1000"   # Defining the default port range
     timeout: int = 60       # Defining a timout after 60 seconds of no response
+
+    @validator('target_url') #This is used to validate the incoming url
+    def normalize_url(cls, v): # This is a method to normalize the url, cls is used to access the class itself and v is the value(URL)
+        return str(v).rstrip('/') # This removes any trailing slashes from the url
