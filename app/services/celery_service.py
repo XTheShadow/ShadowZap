b0b0bb673a7212b2a04420f267ae6953e7e6d41e
XTheShadow <theshadowsamer@gmail.com>
2025-03-19 16:35:35 +0300

    Added a backend service handler -Redis- too

--- app/services/celery_service.py
+++ app/services/celery_service.py
@@ -3,5 +3,6 @@ from celery import Celery
 
 celery = Celery(
     'Automated_Pen_Testing',
-    broker= 'redis://localhost:6379' # The port "6379" is the standard port for Redis 
+    broker='redis://localhost:6379',  # The port "6379" is the standard port for Redis
+    backend='redis://localhost:6379'  # Configure Redis as the result backend
 )
