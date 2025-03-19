cd6213e7fb758ceac605999cc15ae29f61832be4
XTheShadow <theshadowsamer@gmail.com>
2025-03-19 17:44:51 +0300

    Added an -Autodiscovery mdethod- for tasks module

--- app/services/celery_service.py
+++ app/services/celery_service.py
@@ -4,5 +4,9 @@ from celery import Celery
 celery = Celery(
     'Automated_Pen_Testing',
     broker='redis://localhost:6379',  # The port "6379" is the standard port for Redis
-    backend='redis://localhost:6379'  # Configure Redis as the result backend
+    backend='redis://localhost:6379',  # Configure Redis as the result backend
+    include=['app.tasks.scan_tasks']  # Including the tasks module for registration
 )
+
+# Configuring Celery to auto-discover tasks
+celery.autodiscover_tasks(['app.tasks'])
